b38d3bf1f2562109f3137b67a2c32aa32959ba27
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 11:38:53 -0300

    react

--- src/componentes/Produto.jsx
+++ src/componentes/Produto.jsx
@@ -3,10 +3,12 @@ import {useState} from 'react'
 
 function Produto() {
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [lista, setLista] = useState([]);
 
   const incrementar = ()=>setCount(count + 1)
   const decrementar = ()=>setCount(count - 1)
+  const adicionarLista = ()=>setLista([...lista, Math.random(10).toFixed(2)])
 
 
 
@@ -18,6 +20,14 @@ function Produto() {
       <p>Valor:{count}</p>
       <button onClick={incrementar}>Aumentar</button>
       <button onClick={decrementar}>Diminuir</button>
+     </fieldset>
+
+     <fieldset>
+      <legend>Exemplo2</legend>
+      <ul>
+        {lista.map((n,i)=><li key={i}>{n}</li>)}
+      </ul>
+      <button onClick={adicionarLista}>Lista</button>
      </fieldset>
     </>
   )
